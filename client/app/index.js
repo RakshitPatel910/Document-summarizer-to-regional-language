18b4bb7bd0b02246e9ff53c07f8d9fa41c228e2c
RakshitPatel910 <86058505+RakshitPatel910@users.noreply.github.com>
2023-10-14 13:28:43 +0530

    native app setup

---
+++ client/app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Layout = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Layout;
